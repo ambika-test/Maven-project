2d4608d93b7e016f6481b31517246d43659ba5de
Admin <Admin@DESKTOP-KBOOFVV>
2024-12-26 19:06:18 +0530

    modification done inmaven

--- Maven/src/main/java/com/benzakin/pom/LoginPage.java
+++ Maven/src/main/java/com/benzakin/pom/LoginPage.java
@@ -1,5 +1,18 @@
 package com.benzakin.pom;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class LoginPage {
+	@Test
+	public void login()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.instagram.com");
+		driver.quit();
+		
+	}
 
 }
